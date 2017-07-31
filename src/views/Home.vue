0bcc64f5a7d068bcc9ed39920c1c5c19b27bfeac
Seth Davis <seth.davis@egov.com>
2017-07-31 18:51:26 -0500

    Delete google js style guide. Steal air bnb's. Sort of.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,201 @@
 <template>
-    <div class="home-container content">        
-        <app-table-of-contents
-            :companyName="companyName">
-        </app-table-of-contents>
-        <hr>
-        <div id="content">
-            <app-intro :companyName="companyName"></app-intro>
-            <hr>
-            <app-source-file></app-source-file>
-            <hr>
-            <app-formatting></app-formatting>
-            <hr>
-            <app-language-features></app-language-features>
-            <hr>
-            <app-naming></app-naming>
-            <hr>
-            <app-react-jsx-standard></app-react-jsx-standard>
-            <hr>
-            <app-jest-standard></app-jest-standard>
-            <hr>
-            <app-policies></app-policies>
-            <hr>
-            <app-appendices></app-appendices>
-        </div>
+    <div class="home-container content">
+        <section>
+            <table class="table">
+                <tbody>
+                    <tr v-for="headingGroup in dividedHeadings">
+                        <td v-for="(heading, idx) in headingGroup">
+                            <a :href="'#' + heading.id" v-html="heading.content"></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+        <section>
+            <h3 id="types">
+                Types
+            </h3>
+            <p id="types-primitive">
+                <strong>Primitives</strong> - When accessing a primitive type, always work directly with its value.
+            </p>
+            <ul>
+                <li>string</li>
+                <li>number</li>
+                <li>boolean</li>
+                <li>null</li>
+                <li>undefined</li>
+            </ul>
+            <pre><code>const foo = 1;<br>let bar = foo;<br><br>bar = 9;<br>console.log(foo, bar); // => 1, 9</code></pre>
+        </section>
+        <section>
+            <h3 id="references">
+                References
+            </h3>
+        </section>
+        <section>
+            <h3 id="objects">
+                Objects
+            </h3>
+        </section>
+        <section>
+            <h3 id="arrays">
+                Arrays
+            </h3>
+        </section>
+        <section>
+            <h3 id="destructuring">
+                Destructuring
+            </h3>
+        </section>
+        <section>
+            <h3 id="strings">
+                Strings
+            </h3>
+        </section>
+        <section>
+            <h3 id="functions">
+                Functions
+            </h3>
+        </section>
+        <section>
+            <h3 id="arrow-functions">
+                Arrow Functions
+            </h3>
+        </section>
+        <section>
+            <h3 id="classes-and-constructors">
+                Classes & Constructors
+            </h3>
+        </section>
+        <section>
+            <h3 id="modules">
+                Modules
+            </h3>
+        </section>
+        <section>
+            <h3 id="iterators-and-generators">
+                Iterators and Generators
+            </h3>
+        </section>
+        <section>
+            <h3 id="properties">
+                Properties
+            </h3>
+        </section>
+        <section>
+            <h3 id="variables">
+                Variables
+            </h3>
+        </section>
+        <section>
+            <h3 id="hoisting">
+                Hoisting
+            </h3>
+        </section>
+        <section>
+            <h3 id="comparison-operators-and-equality">
+                Comparison Operators & Equality
+            </h3>
+        </section>
+        <section>
+            <h3 id="blocks">
+                Blocks
+            </h3>
+        </section>
+        <section>
+            <h3 id="control-statements">
+                Control Statements
+            </h3>
+        </section>
+        <section>
+            <h3 id="comments">
+                Comments
+            </h3>
+        </section>
+        <section>
+            <h3 id="whitespace">
+                Whitespace
+            </h3>
+        </section>
+        <section>
+            <h3 id="commas">
+                Commas
+            </h3>
+        </section>
+        <section>
+            <h3 id="semicolons">
+                Semicolons
+            </h3>
+        </section>
+        <section>
+            <h3 id="type-casting-and-coercion">
+                Type Casting & Coercion
+            </h3>
+        </section>
+        <section>
+            <h3 id="naming-conventions">
+                Naming Conventions
+            </h3>
+        </section>
+        <section>
+            <h3 id="accessors">
+                Accessors
+            </h3>
+        </section>
+        <section>
+            <h3 id="events">
+                Events
+            </h3>
+        </section>
+        <section>
+            <h3 id="es5-compatibility">
+                ECMAScript 5 Compatibility
+            </h3>
+        </section>
+        <section>
+            <h3 id="es6-styles">
+                ECMAScript 6+ (ES 2015+) Styles
+            </h3>
+        </section>
+        <section>
+            <h3 id="testing">
+                Testing
+            </h3>
+        </section>
+        <section>
+            <h3 id="performance">
+                Performance
+            </h3>
+        </section>
     </div>
 </template>
 
 <script>
 
-    import AppTableOfContents from '../components/static/sections/TableOfContents.vue'
-    import AppIntro from '../components/static/sections/Intro.vue'
-    import AppSourceFile from '../components/static/sections/SourceFile.vue'
-    import AppFormatting from '../components/static/sections/Formatting.vue'
-    import AppLanguageFeatures from '../components/static/sections/LanguageFeatures.vue'
-    import AppNaming from '../components/static/sections/Naming.vue'
-    import AppPolicies from '../components/static/sections/Policies.vue'
-    import AppAppendices from '../components/static/sections/Appendices.vue'
-    import AppReactJsxStandard from '../components/static/sections/ReactJsxStandard.vue'
-    import AppJestStandard from '../components/static/sections/JestStandard.vue'
+    import { chunk } from 'lodash'
 
     export default {
-        props: ['companyName'],
+        props: [],
+        mounted() {
+            this.$el.querySelectorAll('h3').forEach(
+                heading => this.headings.push({
+                    id: heading.id,
+                    content: heading.innerHTML
+                })
+            )
+        },
+        data() {
+            return {
+                headings: []
+            }
+        },
+        computed: {
+            dividedHeadings() {
+                return chunk(this.headings, 3)
+            }
+        },
         components: {
-            AppTableOfContents,
-            AppIntro,
-            AppSourceFile,
-            AppFormatting,
-            AppLanguageFeatures,
-            AppNaming,
-            AppPolicies,
-            AppAppendices,
-            AppReactJsxStandard,
-            AppJestStandard
+
         }
     }
 
