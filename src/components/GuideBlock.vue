4f1d0cd7810b252c529f8322a49a1812f4463d1b
Seth Davis <seth.davis@egov.com>
2017-07-31 19:20:49 -0500

    Update bulma. Add guide block component. Begin the madness.

---
+++ src/components/GuideBlock.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="guide-block-container">
+        <h3 :id="id">
+            {{ title }}
+        </h3>
+        <slot></slot>
+        <hr>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: ['id', 'title']
+    }
+</script>
+
+<style lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home-container content">
-        <section>
+        <guide-block>
             <table class="table">
                 <tbody>
                     <tr v-for="headingGroup in dividedHeadings">
@@ -10,11 +10,8 @@
                     </tr>
                 </tbody>
             </table>
-        </section>
-        <section>
-            <h3 id="types">
-                Types
-            </h3>
+        </guide-block>
+        <guide-block id="types" title="Types">
             <p id="types-primitive">
                 <strong>Primitives</strong> - When accessing a primitive type, always work directly with its value.
             </p>
@@ -26,152 +23,332 @@
                 <li>undefined</li>
             </ul>
             <pre><code>const foo = 1;<br>let bar = foo;<br><br>bar = 9;<br>console.log(foo, bar); // => 1, 9</code></pre>
-        </section>
-        <section>
-            <h3 id="references">
-                References
-            </h3>
-        </section>
-        <section>
+            <p id="types-complex">
+                <strong>Complex</strong> - When accessing a primitive type, always work directly with its value.
+            </p>
+            <ul>
+                <li>object</li>
+                <li>array</li>
+                <li>function</li>
+            </ul>
+            <pre><code>const foo = [1, 2];<br>const bar = foo;<br><br>bar[0] = 9;<br>console.log(foo[0], bar[0]); // => 9, 9</code></pre>
+        </guide-block>
+        <guide-block
+            id="references"
+            title="References">
+            <p>
+                Use <code>const</code> for all your references; avoid using <code>var</code>.
+            </p>
+            <pre><code>// bad<br>var a = 1;<br>var b = 2;<br><br>// good<br>const a = 1;
+const b = 2;</code></pre>
+            <p>
+                If you must reassign references, use <code>let</code> instead of <code>var</code>.
+            </p>
+            <pre><code>// bad<br>var count = 1;<br>if (true) {<br>  count += 1;<br>}</code></pre>
+        </guide-block>
+        <guide-block>
             <h3 id="objects">
                 Objects
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="arrays">
                 Arrays
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="destructuring">
                 Destructuring
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="strings">
                 Strings
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="functions">
                 Functions
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="arrow-functions">
                 Arrow Functions
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="classes-and-constructors">
                 Classes & Constructors
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="modules">
                 Modules
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="iterators-and-generators">
                 Iterators and Generators
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="properties">
                 Properties
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="variables">
                 Variables
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="hoisting">
                 Hoisting
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="comparison-operators-and-equality">
                 Comparison Operators & Equality
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="blocks">
                 Blocks
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="control-statements">
                 Control Statements
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="comments">
                 Comments
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="whitespace">
                 Whitespace
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="commas">
                 Commas
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="semicolons">
                 Semicolons
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="type-casting-and-coercion">
                 Type Casting & Coercion
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="naming-conventions">
                 Naming Conventions
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="accessors">
                 Accessors
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="events">
                 Events
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="es5-compatibility">
                 ECMAScript 5 Compatibility
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="es6-styles">
                 ECMAScript 6+ (ES 2015+) Styles
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="testing">
                 Testing
             </h3>
-        </section>
-        <section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
+        <guide-block>
             <h3 id="performance">
                 Performance
             </h3>
-        </section>
+            <p>
+                <strong>Sub-title</strong>
+                Text and instructions go here.
+            </p>
+            <pre><code></code></pre>
+            <hr>
+        </guide-block>
     </div>
 </template>
 
 <script>
-
+    
+    import GuideBlock from '../components/GuideBlock.vue'
     import { chunk } from 'lodash'
 
     export default {
@@ -195,7 +372,7 @@
             }
         },
         components: {
-
+            GuideBlock
         }
     }
 
