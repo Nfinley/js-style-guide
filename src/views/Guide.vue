0044973949cedaa5f953e1fc698cc1124b172f6b
Seth Davis <seth.davis@egov.com>
2017-08-02 18:27:14 -0500

    Rip out crufty html. Add markdown-loader. Import air bnb style guide.

---
+++ src/views/Guide.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="guide-container content">
+        <div id="guide" v-html="content"></div>
+    </div>
+</template>
+
+<script>
+
+    import markdown from '../data/air-bnb-style-guide.md'
+
+    export default {
+        mounted() {
+            document.querySelectorAll('pre').forEach(block => block.className += 'prettyprint');
+        },
+        data() {
+            return {
+                content: markdown
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss">
+
+    pre {
+        border-radius: 4px;
+    }
+
+</style>
